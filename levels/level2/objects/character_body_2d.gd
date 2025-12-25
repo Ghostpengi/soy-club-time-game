28b9762ca78130676aa0a1ea0b6343c5e2d66d20
Imre-L <47687140+Ghostpengi@users.noreply.github.com>
2025-12-25 13:34:57 +1300

    Worked on rope

--- levels/level2/objects/character_body_2d.gd
+++ levels/level2/objects/character_body_2d.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var my_area_shape : Area2D = get_node("Area2D")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,6 +16,8 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -24,3 +27,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	#Checks routinely if the player is overlapping with any collectibles or 
+	#similar interactables
+	if my_area_shape.has_overlapping_areas():
+		var my_area_list = my_area_shape.get_overlapping_areas()
+		print(my_area_list)
+	
+		
+	
+	
